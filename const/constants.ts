ff97809a674269def9d5b00d5282272c71b255a8
clubdrop <94155502+hashbomb@users.noreply.github.com>
2023-09-19 10:45:21 +0800

    updated addr

--- const/constants.ts
+++ const/constants.ts
@@ -2,11 +2,11 @@ import { Mumbai, Chain } from '@thirdweb-dev/chains'
 
 // your token bound factory address
 export const factoryAddress: string = '0x02101dfB77FDE026414827Fdc604ddAF224F0921'
-export const implementation: string = '0xf5df23e81cfD6c5FB25A1dDE9a4a80c03B7e7F39'
+export const implementation: string = '0x13eA85eb0A4AFc3d98F9Fe13FB943C0EF3447e63'
 
 // Your thirdweb api key - you can get one at https://thirdweb.com/dashboard/api-keys
-export const TWApiKey: string = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || ''
+export const TWApiKey: string = '208d2956b4817380a3b6f94abe741cfd'
 export const activeChain: Chain = Mumbai
 
-export const nftDropAddress: string = '0xE0EDdB4df871a023c3e72E72185c3ccbF6F2E58F'
-export const tokenAddress: string = '0x006eF094e2DF803074Ef7Bce35994771a87a6fca'
+export const nftDropAddress: string = '0x04487C284a2b0aD51c1690A225eE7c1205Ee45Bf'
+export const tokenAddress: string = '0x6Fee202429393432B72436AB7334C674884E3f5e'
